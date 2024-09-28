d7d09caad219c83709baa1d04624d326a990894d
hieunhan1919 <hieunhan9191@gmail.com>
2024-09-28 23:26:42 +0700

    BaiTapReactLTTBDD Tuan 5

---
+++ App.js
@@ -0,0 +1,87 @@
+// // App.js
+
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import Screen_1 from './screens/Screen_1';
+// import Screen_2 from './screens/Screen_2';
+// import Screen_3 from './screens/Screen_3';
+
+
+
+// const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Screen_1">
+//         <Stack.Screen
+//           name="Screen_1"
+//           component={Screen_1}
+//           options={{ headerShown: false }} // Ẩn header mặc định
+//         />
+//         <Stack.Screen
+//           name="Screen_2"
+//           component={Screen_2}
+//           options={{ headerShown: false }} // Ẩn header mặc định
+//         />
+//         <Stack.Screen
+//           name="Screen_3"
+//           component={Screen_3}
+//           options={{ headerShown: false }} // Ẩn header mặc định
+//         />
+        
+       
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+// App.js
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Screen_1 from './screens/Screen_1';
+import Screen_2 from './screens/Screen_2';
+import Screen_3 from './screens/Screen_3';
+import Screen_4 from './screens/Screen_4';
+
+
+
+
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Screen_1">
+        <Stack.Screen
+          name="Screen_1"
+          component={Screen_1}
+          options={{ headerShown: false }} // Ẩn header mặc định
+        />
+        <Stack.Screen
+          name="Screen_2"
+          component={Screen_2}
+          options={{ headerShown: false }} // Ẩn header mặc định
+        />
+        <Stack.Screen
+          name="Screen_3"
+          component={Screen_3}
+          options={{ headerShown: false }} // Ẩn header mặc định
+        />
+        <Stack.Screen
+          name="Screen_4"
+          component={Screen_4}
+          options={{ headerShown: false }} // Ẩn header mặc định
+        />
+       
+
+        
+       
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
